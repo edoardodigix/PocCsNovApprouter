9e3cb8e944da38bcdc61b2c56104ba161178e8fa
Gabriele Omegna <gabriele.omegna@digixplus.it>
2024-06-20 14:59:15 +0000

    fixed main view controller

--- poccsnovsrvcld-frontend/webapp/controller/MainView.controller.js
+++ poccsnovsrvcld-frontend/webapp/controller/MainView.controller.js
@@ -14,14 +14,15 @@ function (Controller, JSONModel) {
             const requestedView = oEvent.getSource().getSelectedItem().getText();
             if (requestedView === 'Ordine di vendita') {
                 this.getView().byId("odv-view").setVisible(true);
-                this.getView().byId("doccon-view").setVisible(false);
-            } else if (requestedView === 'Documento di consegna') {
-                this.getView().byId("doccon-view").setVisible(true);
+                this.getView().byId("docconsprod-view").setVisible(false);
+            } else if (requestedView === 'Documento spedizione prodotto') {
                 this.getView().byId("odv-view").setVisible(false);
+                this.getView().byId("docconsprod-view").setVisible(true);
             } else {
-                this.getView().byId("doccon-view").setVisible(false);
                 this.getView().byId("odv-view").setVisible(false);
+                this.getView().byId("docconsprod-view").setVisible(false);
             }
+
         },
     });
 });
