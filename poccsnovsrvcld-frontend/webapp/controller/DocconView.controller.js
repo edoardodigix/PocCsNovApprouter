3edc67f3f9b955973f882e3e465b4fe03f4e3d3c
Gabriele Omegna <gabriele.omegna@digixplus.it>
2024-06-21 09:16:13 +0000

    inserite le funzioni per inserire i record nella tabella riferimenti

--- poccsnovsrvcld-frontend/webapp/controller/DocconView.controller.js
+++ poccsnovsrvcld-frontend/webapp/controller/DocconView.controller.js
@@ -32,142 +32,6 @@ function (Controller, JSONModel, Filter, FilterOperator,
             // DEFINIZIONE DEI COMPONENTI CHE RICHIAMEREMO TANTE VOLTE NEL CODICE
             this.oFiltersTable = this.getView().byId("filters-table");
         },
-
-        onOpenPdf: function (oEvent) {
-            const pdfViewer = new sap.m.PDFViewer();
-            this.getView().addDependent(pdfViewer);
-            const odvNumber = oEvent.getSource().getParent().getParent().getCells()[0].getText();
-
-            const sSource = `./res/ODV_${odvNumber}.pdf`
-            pdfViewer.setSource(sSource);
-            pdfViewer.setTitle("My Custom Title");
-            pdfViewer.open();
-        },
-
-        onReset: function () {
-            console.log("STOP");
-            const filterInputs = this.getView().getControlsByFieldGroupId("filtri-input").filter(c => c.isA("sap.m.MultiComboBox") || c.isA ("sap.m.DateRangeSelection"));
-            let mulComInputs = [] ;
-            let dateInputs = [];
-            filterInputs.forEach((input)=> {
-                if (input.isA("sap.m.MultiComboBox") == true){
-                    mulComInputs.push(input);
-                } else if (input.isA("sap.m.DateRangeSelection") == true) {
-                    dateInputs.push(input);
-                }
-            });
-            mulComInputs.forEach((input)=> {
-                input.removeAllSelectedItems();
-            })
-
-            dateInputs.forEach((dateRange) => {
-                dateRange.setDateValue(null);
-                dateRange.setSecondDateValue(null);
-            })
-            this.getView().byId("filtri-btn").setEnabled(false);
-            // this.getView().byId('table-odv');
-
-        },
-
-        onSelectRow: function(oEvent) {
-            const aIndices = oEvent.getSource().getSelectedIndices();
-            const oSelectButton = this.getView().byId("table-odv-btn-seleziona");
-            if (aIndices.length > 0)
-                oSelectButton.setEnabled(true);
-            else
-                oSelectButton.setEnabled(false);
-        },
-
-        onSelectButtonPress: function() {
-            const aRows = this.getView().byId("table-odv").getRows();
-            let selectedData = this.getView().getModel().getData().selectedData;
-            let newRowCells;
-            let newData = {};
-            const aIndices = this.getView().byId("table-odv").getSelectedIndices();
-
-            aIndices.forEach((index) => {
-                newRowCells = aRows.at(index).getCells();
-                newData.numberODV = newRowCells[0].getProperty("text");
-                newData.deliveryNumber = newRowCells[1].getProperty("text");
-                newData.destinatarioMerci = newRowCells[2].getProperty("text");
-                newData.indirizzoDest = newRowCells[3].getProperty("text");
-                newData.dateUM = newRowCells[5].getProperty("text");
-                newData.no_packages = newRowCells[6].getProperty("text");
-                newData.trasportatore = newRowCells[7].getProperty("text"); 
-                newData.luogoSped = newRowCells[8].getProperty("text");
-                newData.status = newRowCells[9].getProperty("text");
-
-                // Check se il dato è già presente tra i selectedData
-                if (selectedData.length == 0)
-                    selectedData.push(newData);
-                else {
-                    let count = 0;
-                    selectedData.forEach((data) => {
-                        if (newData.numberODV != data.numberODV)
-                            ++count;
-                    });
-                    if (count == selectedData.length)
-                        selectedData.push(newData);
-                }
-                newData = {};
-            });
-
-            // sampleData.selectedData = selectedData;
-            const beforeData = this.getView().getModel().getData().data;
-            this.getView().getModel().setData({'data': beforeData, 'selectedData': selectedData});
-
-            this.getView().byId("table-riferimenti-row-mode").setRowCount(selectedData.length);
-            this.getView().byId("panel-riferimenti").setVisible(true);
-            // Accediamo ad un elemento di un'altra View
-            const ownerId = this.getView()._sOwnerId;
-            const viewName = "MainView";
-            const elementId = "select-tipologia";
-            sap.ui.getCore().byId(`${ownerId}---${viewName}--${elementId}`).setEnabled(false);
-        },
-
-        onInputChange: function () {
-            // Resa la funzione standard per qualsiasi numero di input che abbiamo 
-            const filterInputs = this.getView().getControlsByFieldGroupId("filtri-input").filter(c => c.isA("sap.m.MultiComboBox") || c.isA ("sap.m.DateRangeSelection"));
-            const btn = this.getView().byId("filtri-btn");
-            let mulComInputs = [] ;
-            let dateInputs = [];
-            filterInputs.forEach((input)=> {
-                if (input.isA("sap.m.MultiComboBox") == true){
-                    mulComInputs.push(input);
-                } else if (input.isA("sap.m.DateRangeSelection") == true) {
-                    dateInputs.push(input);
-                }
-            });
-            let selectedCombos = mulComInputs.filter((input) => input.getSelectedItems().length > 0);
-            let selectedDates = dateInputs.filter((dateRange) => dateRange.getDateValue() !== null);
-            if(selectedCombos.length == 0 && selectedDates.length == 0)
-                btn.setEnabled(false);
-            else
-                btn.setEnabled(true);
-        },
-
-        onDeleteRiferimentiPress: function(oEvent) {
-            const oModel = this.getView().getModel();
-            const initialData = oModel.getData();
-            let newData = initialData.selectedData;
-            const rowToRemove = oEvent.getSource().getParent().getParent().getCells()[0].getProperty("text");
-            for (let i = 0; i < newData.length; ++i) {
-                if (initialData.selectedData[i].numberODV === rowToRemove)
-                newData.splice(i, 1);
-            }
-            initialData.selectedData = newData;
-            const beforeData = initialData.selectedData;
-            this.getView().getModel().setData({'data': initialData.data, 'selectedData': beforeData});
-            this.getView().byId("table-riferimenti-row-mode").setRowCount(newData.length);
-            if (newData.length === 0) {
-                this.getView().byId("panel-riferimenti").setVisible(false);
-                const ownerId = this.getView()._sOwnerId;
-                const viewName = "MainView";
-                const elementId = "select-tipologia";
-                sap.ui.getCore().byId(`${ownerId}---${viewName}--${elementId}`).setEnabled(true);
-            }
-        },
-
         onCerca: function() {
             // DEFINIZIONE DEI COMPONENTI CHE RICHIAMEREMO NELLA FUNZIONE
             const oFilterBar = this.getView().byId("filters-bar");
@@ -218,6 +82,86 @@ function (Controller, JSONModel, Filter, FilterOperator,
                 this.oFiltersTable.getParent().setVisible(true);
                 this.oFiltersTable.getRowMode().setRowCount(oFilterTableRows.getCount());
             }
-        }
+        },
+
+        rowSelectionChange: function(oEvent) {
+            const aIndices = oEvent.getSource().getSelectedIndices();
+            const oSelectButton = this.getView().byId("seleziona-btn");
+            if (aIndices.length > 0)
+                oSelectButton.setEnabled(true);
+            else
+            oSelectButton.setEnabled(false);
+        },
+
+        onSeleziona: function() {
+			const aSelectedIndices = this.oFiltersTable.getSelectedIndices();
+			const aSelectedRows = this.oFiltersTable.getRows().filter(row => aSelectedIndices.includes(row.getIndex()));
+            console.log("STOP");
+			aSelectedRows.forEach(row => {
+				// DEFINIAMO L'ARRAY CHE CONTERRA' I NUMEROODV DI TUTTI GLI ELEMENTI GIA' PRESENTI NEL MODELLO JSON DEI RIFERIMENTI
+				const currentRows = this._getArrayNumeriOdvRiferimenti();
+				// TIRIAMO FUORI IL NUMEROODV DELLA RIGA CHE STIAMO AGGIUNGENDO (QUELLA DEL FOREACH)
+				const rowNumeroOdv = this._getNumeroOdvFromRow(row);
+				const aMockData = this.getView().getModel().getData().data;
+				const elementToAdd = aMockData.filter(row => row.numberODV === rowNumeroOdv)[0];
+				if(!currentRows.includes(rowNumeroOdv)) {
+					this.getView().getModel().getData().selectedData.push(elementToAdd);
+				}
+			});
+			this.getView().getModel().refresh(true);
+			this._resetRiferimentiRowCount();
+			this.getView().byId("riferimenti-panel").setVisible(true);
+			this._setSelectTipologiaTo(false);
+		},
+
+        _getArrayNumeriOdvRiferimenti: function () {
+            console.log("STOP");
+			return this.getView().getModel().getData().selectedData.reduce((currentRows, row) => {
+				currentRows.push(row.numberODV);
+				return currentRows;
+			}, []);
+		},
+
+        _getNumeroOdvFromRow: function (row) {
+			return row.getCells().reduce((finalValue, cell) => {
+				if (cell.getBindingPath("text") === 'numberODV')
+					finalValue = cell.getText();
+				return finalValue;
+			}, "");
+		},
+
+        onReset: function () {
+            console.log("STOP");
+            const filterInputs = this.getView().getControlsByFieldGroupId("filtri-input").filter(c => c.isA("sap.m.MultiComboBox") || c.isA ("sap.m.DateRangeSelection"));
+            let mulComInputs = [] ;
+            let dateInputs = [];
+            filterInputs.forEach((input)=> {
+                if (input.isA("sap.m.MultiComboBox") == true){
+                    mulComInputs.push(input);
+                } else if (input.isA("sap.m.DateRangeSelection") == true) {
+                    dateInputs.push(input);
+                }
+            });
+            mulComInputs.forEach((input)=> {
+                input.removeAllSelectedItems();
+            })
+
+            dateInputs.forEach((dateRange) => {
+                dateRange.setDateValue(null);
+                dateRange.setSecondDateValue(null);
+            })
+            this.getView().byId("filtri-btn").setEnabled(false);
+            // this.getView().byId('table-odv');
+        },
+        onOpenPdf: function (oEvent) {
+            const pdfViewer = new sap.m.PDFViewer();
+            this.getView().addDependent(pdfViewer);
+            const odvNumber = oEvent.getSource().getParent().getParent().getCells()[0].getText();
+
+            const sSource = `./res/ODV_${odvNumber}.pdf`
+            pdfViewer.setSource(sSource);
+            pdfViewer.setTitle("My Custom Title");
+            pdfViewer.open();
+        },
     });
 });
